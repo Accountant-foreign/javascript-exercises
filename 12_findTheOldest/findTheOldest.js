92a290404ad7bd9334ea969a61c60eb2c821b612
Accountant-foreign <mdh2000@hw.ac.uk>
2022-12-25 09:40:58 +0400

    Get current date

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -3,7 +3,7 @@ const findTheOldest = function() {
     let obj=objArr.map((objArr)=>{
         if("yearOfDeath" in objArr) return {name: objArr.name, age: objArr.yearOfDeath-objArr.yearOfBirth};
         else {
-            return {name: objArr.name, age: 2022-objArr.yearOfBirth}
+            return {name: objArr.name, age: (new Date().getFullYear())-objArr.yearOfBirth}
         }
     });
     return obj.sort((a,b)=> a.age-b.age)[obj.length-1];
