93f9ea3133d104aa311203b47303df1df411306e
Accountant-foreign <mdh2000@hw.ac.uk>
2022-12-25 09:38:41 +0400

    Shortening code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,15 +1,12 @@
 const findTheOldest = function() {
     let objArr=arguments[0];
-    // let obj=objArr.map((objArr) => ({name: objArr.name, age: objArr.yearOfDeath-objArr.yearOfBirth}));
     let obj=objArr.map((objArr)=>{
-        if("yearOfDeath" in objArr){
-            return {name: objArr.name, age: objArr.yearOfDeath-objArr.yearOfBirth};
-        } else {
+        if("yearOfDeath" in objArr) return {name: objArr.name, age: objArr.yearOfDeath-objArr.yearOfBirth};
+        else {
             return {name: objArr.name, age: 2022-objArr.yearOfBirth}
         }
     });
-    obj.sort((a,b)=> a.age-b.age);
-    return obj[obj.length-1];
+    return obj.sort((a,b)=> a.age-b.age)[obj.length-1];
 };
 
 // Do not edit below this line
