e66a5efe00e9f8a7c25c9771315d75857a7668c5
Accountant-foreign <mdh2000@hw.ac.uk>
2022-12-25 09:31:51 +0400

    Finding the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
 const findTheOldest = function() {
-
+    let objArr=arguments[0];
+    // let obj=objArr.map((objArr) => ({name: objArr.name, age: objArr.yearOfDeath-objArr.yearOfBirth}));
+    let obj=objArr.map((objArr)=>{
+        if("yearOfDeath" in objArr){
+            return {name: objArr.name, age: objArr.yearOfDeath-objArr.yearOfBirth};
+        } else {
+            return {name: objArr.name, age: 2022-objArr.yearOfBirth}
+        }
+    });
+    obj.sort((a,b)=> a.age-b.age);
+    return obj[obj.length-1];
 };
 
 // Do not edit below this line
